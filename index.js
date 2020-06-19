83f284d1fce7d5cd73ca2607c0682df9c9ac8442
JiriKofron <51482900+JiriKofron@users.noreply.github.com>
2020-06-20 00:35:06 +0200

    Prvni porod
    Navržená struktura, složky, zavrhnutý carusel na social proof

---
+++ index.js
@@ -0,0 +1,11 @@
+const proof = document.querySelector(".social-proof");
+const prevBtn = document.getElementById("prev-btn");
+const index = 0;
+
+prevBtn.addEventListener("click", () => {
+        
+        proof.style.transform = "translateX(-10rem)";
+        
+    
+    });
+ 

ce1438eedb99bd5427e8518aedccb5e1de6d37a7
Jiří Kofroň <jiri.kofron@signi.com>
2022-04-07 01:30:21 +0200

    new version and boilerplate with Vue3 and typescript

---
+++ bara-web/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import TheNavbar from "@/components/TheNavbar.vue";
+
+export default {
+  components: {TheNavbar},
+  data() {
+    return {}
+  }
+}
+
+</script>
+
+<template>
+  <TheNavbar/>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+</style>
---
+++ bara-web/src/components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Menu</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toogle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TheNavbar"
+}
+</script>
+
+<style scoped>
+
+</style>
